fd71d4aedf0a7319c32485953f10a658de092b79
soumyabanerjee <soumyabanerjee@soumyabanerjees-MacBook-Pro-257.local>
2023-12-29 09:41:12 +0000

    edits automated git

--- npjcomment.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-./compile_latex.sh cover_letter_AISociety.tex; cp cover_letter_AISociety.pdf ~/Downloads/npj_mh_comment/; cp AI_society_paper.pdf ~/Downloads/npj_mh_comment;  cd ~/Downloads/npj_mh_comment; mv cover_letter_AISociety.pdf cover_letter.pdf; mv AI_society_paper.pdf comment_npj.pdf;  cd ~/cam_project/manuscript ;
-
-
