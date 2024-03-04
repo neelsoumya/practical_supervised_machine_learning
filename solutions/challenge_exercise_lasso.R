3c5fd2f6f4114148341108c36fb88c4bc590b149
soumyabanerjee <soumyabanerjee@soumyabanerjees-MacBook-Pro-652.local>
2024-03-04 10:31:37 +0000

    edits automated git

--- solutions/challenge_exercise_lasso.R
+++ solutions/challenge_exercise_lasso.R
@@ -6,4 +6,36 @@
 # install.packages('glmnet')
 ###############################################
 
+###################
+# load library
+###################
 library(glmnet)
+
+#############
+# load data
+#############
+data("BinomialExample")
+
+x <- BinomialExample$x
+y <- BinomialExample$y
+
+
+#############################
+# fit LASSO model
+#############################
+fit_lasso <- glmnet::glmnet(x, y, family = "binomial")
+
+###########
+# predict
+###########
+predict.glmnet(object = fit_lasso, newx = x[1:5,], type = "class", s=c(0.05))
+
+#################
+# cross-validate
+#################
+cvfit_lasso = cv.glmnet(x, y, family="binomial", type.measure = "class")
+
+################################
+# plot cross-validation curve
+################################
+plot(cvfit_lasso)
