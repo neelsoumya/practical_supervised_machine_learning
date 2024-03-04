dc97d122c6fe508e20cf376bc878074790e5d20e
soumyabanerjee <soumyabanerjee@soumyabanerjees-MacBook-Pro-652.local>
2024-03-04 10:24:34 +0000

    edits automated git

---
+++ solutions/challenge_exercise_lasso.R
@@ -0,0 +1,9 @@
+###############################################
+# simple example of LASSO logistic regression
+# 
+# https://glmnet.stanford.edu/articles/glmnet.html#logistic-regression-family-binomial
+#
+# install.packages('glmnet')
+###############################################
+
+library(glmnet)
